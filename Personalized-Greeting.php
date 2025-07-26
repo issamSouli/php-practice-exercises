c68ff1a0d8ce0af4f57a65cb06326220c7e4aee6
issam-wp-support <issamsoulimani25@gmail.com>
2025-07-26 23:22:22 +0100

    Create Personalized-Greeting.php

---
+++ Personalized-Greeting.php
@@ -0,0 +1,14 @@
+<!--
+* Declare two string variables: one for first name, one for last name.
+* Combine them with exactly one space into a third variable.
+* Output a single greeting sentence that uses the full name.
+-->
+<?php
+
+$firstName = "issam";
+$lastName = "soulimani";
+$fullName = $firstName ." ".$lastName;
+
+echo "Hello, $fullName, Nice to meet you.";
+
+?>
