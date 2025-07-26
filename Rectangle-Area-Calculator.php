9cb68e63908304ef515329331ad7c71408aef599
issam-wp-support <issamsoulimani25@gmail.com>
2025-07-26 18:29:50 +0100

    Create Rectangle-Area-Calculator.php

---
+++ Rectangle-Area-Calculator.php
@@ -0,0 +1,17 @@
+<!--
+-- Make two variables:$width – give it any whole-number value you like.$height – give it any whole-number value you like.
+-- Calculate the area of the rectangle :area = width × height
+-- Store the result in a variable called $area.
+-- Use echo to print a friendly sentence that shows the width, height, and area, such as:A rectangle with width 4 and height 3 has an area of 12.
+-->
+
+<?php
+
+$width = 25;
+$height = 30;
+
+$area = $width * $height;
+
+echo "A rectangle with width $width and height $height has an area of $area.";
+
+?>
