a87ee565589f34c1887d4b739b2d5f60bfb2844e
issam-wp-support <110335623+issamSouli@users.noreply.github.com>
2025-07-26 17:55:54 +0100

    Update sum-of-two-numbers.php

--- sum-of-two-numbers.php
+++ sum-of-two-numbers.php
@@ -1,10 +1,5 @@
-<!--problem-2 :
-** Create a new PHP file (for example, add.php).
-** Inside the file, make two variables, $a and $b.
-** Give them any whole-number values you like (for example, 7 and 5).
-** Calculate the sum of those two numbers.
-** Use echo to print a sentence that says what the two numbers are and what their sum is.
-** It should look a bit like: “The sum of 7 and 5 is 12.” -->
+<!--problem-2 : Create a PHP file, add two numbers, and echo “The sum of X and Y.” -->
+
 <?php
 $a = 7;
 $b = 8;
