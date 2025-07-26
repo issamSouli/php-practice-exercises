8d7641eecc7485b1c0455e8774d20fc3267429b9
issam-wp-support <issamsoulimani25@gmail.com>
2025-07-26 22:06:26 +0100

    Create Average-of-Three-Numbers.php

---
+++ Average-of-Three-Numbers.php
@@ -0,0 +1,13 @@
+<!--
+Create three variables $a, $b, and $c with any whole‑number values, calculate their average , store it in $average, and echo a sentence showing the three numbers and their average.
+-->
+<?php
+$a = 10;
+$b = 15;
+$c = 20;
+$sum = $a + $b + $c;
+$average = $sum / 3;
+
+echo "The average of the numbers $a , $b and $c is $average.";
+
+?>
