6b0f97ab3ea1e85adc64bb9a75674cce14f9b519
issam-wp-support <issamsoulimani25@gmail.com>
2025-07-27 14:29:09 +0100

    Create Currency-Converter(USD → MAD).php

---
+++ Currency-Converter(USD → MAD).php
@@ -0,0 +1,13 @@
+<!--Write a PHP script that declares $usd and $rate, computes $mad = $usd * $rate, and uses echo to display the conversion.-->
+
+<?php
+
+$usd = 1213;
+$rate = 8.99;
+
+$mad = $usd * $rate;
+
+echo "you will receive $usd in dollar, which equal $mad in Morrocan Dirham.";
+
+
+?>
