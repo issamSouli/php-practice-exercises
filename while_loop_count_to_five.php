9b099668ee68cc82c5e91ae6dff4ae0a1029a4d3
issam-wp-support <issamsoulimani25@gmail.com>
2025-07-29 16:00:16 +0100

    Create while_loop_count_to_five.php

---
+++ while_loop_count_to_five.php
@@ -0,0 +1,22 @@
+<!--
+Make one variable called $number and set it to 1.
+
+Write a while loop that runs while $number is less than or equal to 5.
+
+Inside the loop, echo the value of $number followed by a space.
+
+Then add 1 to $number.
+
+When the loop finishes you should see: 1 2 3 4 5 ;
+-->
+<?php
+
+$number = 1;
+
+while( $number <= 5){
+    echo $number . " ";
+    $number++;
+}
+
+
+?>
