44148d5a79d7cdcda7d24d98647d9cb4d551e4c4
issam-wp-support <issamsoulimani25@gmail.com>
2025-07-30 16:32:48 +0100

    Create function_Say_Goodbye.php

---
+++ function_Say_Goodbye.php
@@ -0,0 +1,17 @@
+<!--
+Create a function called sayGoodbye.
+The function should take one parameter: $name.
+Inside the function, use echo to display:
+ "Goodbye, [name]. See you soon!"
+
+Call the function with any name you like (e.g., "Issam").
+OUTPUT : Goodbye, Issam. See you soon!
+-->
+<?php
+
+function sayGoodBye($name){
+    echo "Goodbye, $name. See you soon!";
+}
+sayGoodBye("issam");
+
+?>
