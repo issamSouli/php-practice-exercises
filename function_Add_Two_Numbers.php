1043e0e33fcec568f141377194d11c203c06aff4
issam-wp-support <issamsoulimani25@gmail.com>
2025-07-30 15:44:39 +0100

    Create function_Add_Two_Numbers.php

---
+++ function_Add_Two_Numbers.php
@@ -0,0 +1,18 @@
+<!--
+Write a function called addNumbers.
+
+The function should take two parameters: $a and $b.
+
+Inside the function, return the sum of $a and $b.
+
+After defining the function, call it with any two numbers (like 3 and 7) and use echo to display the result. -->
+<?php
+
+function addNumbers($a, $b){
+    return $a + $b;
+}
+echo "the sum is :" . addNumbers(25, 26);
+
+
+
+?>
