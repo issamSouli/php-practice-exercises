50b7d4fb4ccbcb99d5138379a4ac7237ae2c7192
issam-wp-support <issamsoulimani25@gmail.com>
2025-07-30 15:30:51 +0100

    Create function_Greet_a_User.php

---
+++ function_Greet_a_User.php
@@ -0,0 +1,15 @@
+<!--nstructions:
+-Write a function called greetUser.
+-The function should take one parameter: $name.
+-Inside the function, use echo to print:
+"Hello, [name]! Welcome to our site."
+-After defining the function, call it and pass any name you want (e.g., "Issam").--> 
+<?php
+
+function greetUser($name){
+    echo "Hello, $name! Welcome to our site.";
+}
+
+greetUser("issam");
+
+?>
