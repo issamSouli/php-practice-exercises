86799f2510c5b246f11a64e2b9c5acff24a3bc74
issam-wp-support <issamsoulimani25@gmail.com>
2025-07-31 12:31:04 +0100

    Create array_grocery_list.php

---
+++ array_grocery_list.php
@@ -0,0 +1,16 @@
+<!--
+Create an array called $groceries.
+Add 5 grocery items to the array (like "milk", "bread", etc.).
+Use echo to print a sentence for each item using its index, like:
+👉 "I need to buy [item]
+-->
+<?php
+
+$groceries = ["milk", "fruits", "eggs", "oat", "bread"];
+echo "I need to buy {$groceries[0]}.<br>";
+echo "I need to buy {$groceries[1]}.<br>";
+echo "I need to buy {$groceries[2]}.<br>";
+echo "I need to buy {$groceries[3]}.<br>";
+echo "I need to buy {$groceries[4]}.<br>";
+
+?>
