5e7446697e77e3fdafa42222653bbe032e33e774
issam-wp-support <issamsoulimani25@gmail.com>
2025-07-31 11:56:57 +0100

    Create array_favorite_movies.php

---
+++ array_favorite_movies.php
@@ -0,0 +1,18 @@
+<!--
+Create an array called $movies.
+* Add 4 of your favorite movie titles as strings into the array.
+* Use echo to print each movie one by one using their index:
+    - First movie: $movies[0]
+    - Second movie: $movies[1]
+
+    - …and so on.
+-->
+<?php
+$movies = ["endGame", "donnie-Brasco", "seven", "avengers"];
+echo "my favorites movies are: "."<br>";
+echo "first movie is: " . $movies[0]."."."<br>";
+echo "second movie is: " . $movies[1]."."."<br>";
+echo "third movie is: " . $movies[2]."."."<br>";
+echo "fourth movie is: " . $movies[3]."."."<br>";
+
+?>
