a758f3373bef82a5d9596747788a9f976d9bd76d
issam-wp-support <issamsoulimani25@gmail.com>
2025-07-31 12:30:56 +0100

    Create array_days_of_the_week.php

---
+++ array_days_of_the_week.php
@@ -0,0 +1,18 @@
+<!--
+Create an array called $days.
+*Add the 7 days of the week as strings (starting with "Monday").
+*Use echo to display:
+    --The first day ($days[0])
+    --The last day ($days[6])
+*Then echo this sentence:
+👉 "My favorite day is [any day from the array]" — pick whichever you like.
+-->
+<?php
+
+$days =["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+echo "The first day is {$days[0]}.<br>";
+echo "The last day is {$days[6]}.<br>";
+echo "My favorite day is {$days[3]}.<br>";
+
+
+?>
